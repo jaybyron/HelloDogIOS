edf3afa0118b0ef9e12b654ddb3f512804a8adb9
Diana Lyn Inocencio <dianalyninocencio@Dianas-iMac.attlocal.net>
2014-12-30 21:03:14 -0800

    Initial Commit. Add all Necessry files

--- HolaDog/ViewController.swift
+++ HolaDog/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  HolaDog
 //
-//  Created by Diana Lyn Inocencio on 12/30/14.
+//  Created by Byron Jay Inocencio on 12/30/14.
 //  Copyright (c) 2014 Byron Jay Inocencio. All rights reserved.
 //
 
@@ -10,6 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var inputAge: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBOutlet weak var dogImage: UIImageView!
+    
+    @IBAction func inputButton(sender: AnyObject) {
+        var dogAgeMultiplier = 7;
+        
+        var age = inputAge.text.toInt()
+        
+        if age != nil{
+            
+        
+        var dogAge = dogAgeMultiplier * age!
+        
+        
+        println("USER INPUT: " + inputAge.text)
+        println(dogAge)
+        
+        resultLabel.text = "\(inputAge.text) is \(dogAge) dog years"
+        dogImage.alpha = 1;
+        }
+        
+        else{
+            resultLabel.text = "not a valid age"
+        }
+    }
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
